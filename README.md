b368dc7c332025d14e668728c5d3ae6d7dbba07e
Jared Barboza <jared.m.barboza@gmail.com>
2012-05-22 23:37:07 -0400

    optimizes one of the loops, adds example to readme and adds license

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
-jasonhub
-========
+# JasonHUb
+Github API Library for RequireJS.
 
-Github API Library for RequireJS
+### Example
+Here is an example requirejs module that requests a repo and it's languages from the GitHub API:
+
+```javascript
+define([
+  'github'
+], function( github ){
+  var repo_url = 'repos/'+ user + '/' + name,
+      api_calls = [
+                    { method: repo_url, key: "project" },
+                    { method: repo_url + '/languages', key: "languages" }
+                  ];
+
+  github.get(api_calls, function( model ) {
+    // this code will be called once each api method has been called
+    // model.project will contain the response from the repo JSON call
+    // model.languages will contain the response from the languages JSON call
+  });
+});
+```
--- src/jasonhub.js
+++ src/jasonhub.js
@@ -1,3 +1,21 @@
+/*
+  JasonHub
+  (c) 2012 jared barboza
+  http://codeimpossible.com
+  http://github.com/codeimpossible/jasonhub
+
+  GitHub JSONP API Library for RequireJS
+
+
+  The MIT License (MIT)
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 define(function(){
   var api_url = "https://api.github.com/";
 
@@ -26,7 +44,7 @@ define(function(){
       urls = [];
       if( isArray(method) ) {
         // we can declare a dependency on many api calls
-        for(var i = -1, l = method.length; ++i < method.length; ) {
+        for(var i = -1, l = method.length; ++i < l; ) {
           // build up our api calls
           urls.push( format_url( method[i].method, method[i].data ) );
         }
